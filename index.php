8b86328f39b35726690c79562aaafdd794915f13
Michael <michael.alesiano@gmail.com>
2021-02-25 11:45:22 +0100

    Second push

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
+<?php
+$testo = 'Prima esercitazione PHP';
+$lunghezza = strlen($testo);
+$parola = $_GET['badword'];
+$sostituzione = str_replace($parola,'***',$testo);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Prima esercitazione PHP</title>
+  <title>Primo esercizio in PHP</title>
 </head>
 <body>
-  
+
+  <h1>
+    La lunghezza della variabile $testo è di <?= $lunghezza ?> caratteri
+  </h1>
+
+  <h2>
+    <?= $sostituzione ?>
+  </h2>
 
 </body>
 </html>
